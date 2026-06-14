d91f0c6b0dabc8e56724037001a6129ca29f10d6
Bonthu-Jaswini <bonthujaswinijaswini@gmail.com>
2026-06-14 20:39:56 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
           "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
         },
         body: JSON.stringify({
-          model: "model: "llama-3.1-8b-instant"",
+          model: "llama-3.1-8b-instant",
           messages: [
             {
               role: "system",
